67c9f432c5449ff22302c78137d7185c0db8e514
Kyya <favoursmoe@gmail.com>
2019-12-02 11:17:34 +0800

    d5

--- future.js
+++ future.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let status = "Cancer";
-function future(status) {
+function future(status, duration) {
+  console.log(duration);
   console.log(`Your horoscope sign is ${status}~~`);
 }
